1624ccfaad6680276104489bd81a127ab2630bb6
Anderson Andrade <anderson.asa89@gmail.com>
2015-04-04 03:27:54 -0300

    Add TransformableTrait

---
+++ src/Prettus/Repository/Traits/TransformableTrait.php
@@ -0,0 +1,17 @@
+<?php namespace Prettus\Repository\Traits;
+
+/**
+ * Class TransformableTrait
+ * @package Prettus\Repository\Traits
+ */
+trait TransformableTrait {
+
+    /**
+     * @return array
+     */
+    public function transform()
+    {
+        return $this->toArray();
+    }
+
+}
