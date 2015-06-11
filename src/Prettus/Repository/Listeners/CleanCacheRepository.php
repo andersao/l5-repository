ed755277f4001c27cc73933f6ba62086999b842f
Anderson Andrade <anderson.asa89@gmail.com>
2015-06-11 09:43:14 -0300

    Add Repository envents and Clean Cache Listener

---
+++ src/Prettus/Repository/Listeners/CleanCacheRepository.php
@@ -0,0 +1,77 @@
+<?php namespace Prettus\Repository\Listeners;
+
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Prettus\Repository\Contracts\RepositoryInterface;
+use Prettus\Repository\Events\RepositoryEventBase;
+use Prettus\Repository\Helpers\CacheKeys;
+
+/**
+ * Class CleanCacheRepository
+ * @package Prettus\Repository\Listeners
+ */
+class CleanCacheRepository {
+
+    /**
+     * @var CacheRepository
+     */
+    protected $cache = null;
+
+    /**
+     * @var RepositoryInterface
+     */
+    protected $repository = null;
+
+    /**
+     * @var Model
+     */
+    protected $model = null;
+
+    /**
+     * @var string
+     */
+    protected $action = null;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->cache = app( config('repository.cache.repository','cache') );
+    }
+
+    public function handle(RepositoryEventBase $event)
+    {
+        try
+        {
+            $cleanEnabled = config("repository.cache.clean.enabled",true);
+
+            if( $cleanEnabled )
+            {
+                $this->repository = $event->getRepository();
+                $this->model      = $event->getModel();
+                $this->action     = $event->getAction();
+
+                if( config("repository.cache.clean.on.{$this->action}",true) )
+                {
+                    $cacheKeys        = CacheKeys::getKeys(get_class($this->repository));
+
+                    if( is_array($cacheKeys) )
+                    {
+                        foreach($cacheKeys as $key)
+                        {
+                            $this->cache->forget($key);
+                        }
+                    }
+                }
+            }
+        }
+        catch (\Exception $e)
+        {
+            logger()->error($e->getMessage());
+        }
+    }
+
+}
---
+++ src/Prettus/Repository/Providers/EventServiceProvider.php
@@ -0,0 +1,36 @@
+<?php namespace Prettus\Repository\Providers;
+
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider {
+
+    /**
+     * The event handler mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'Prettus\Repository\Events\RepositoryEntityCreated' => [
+            'Prettus\Repository\Listeners\CleanCacheRepository'
+        ],
+        'Prettus\Repository\Events\RepositoryEntityUpdated' => [
+            'Prettus\Repository\Listeners\CleanCacheRepository'
+        ],
+        'Prettus\Repository\Events\RepositoryEntityDeleted' => [
+            'Prettus\Repository\Listeners\CleanCacheRepository'
+        ]
+    ];
+
+    /**
+     * Register any other events for your application.
+     *
+     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @return void
+     */
+    public function boot(DispatcherContract $events)
+    {
+        parent::boot($events);
+    }
+
+}
