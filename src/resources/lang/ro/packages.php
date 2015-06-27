56e035d2358641141e49a3b430d9f7c826774ee4
Ionut Tanasa <ionutz2k@gmail.com>
2015-06-27 18:11:10 +0200

    adding romanian i18n

---
+++ src/resources/lang/ro/packages.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+    'prettus_laravel_validation_required'=>"Pachet obligatoriu. Instalaţi prettus/laravel-validation ('composer require prettus/laravel-validation'), vă rog.",
+    'league_fractal_required'=>"Pachet obligatoriu. Instalaţi league/fractal ('composer require league/fractal') (0.12.*), vă rog."
+);
