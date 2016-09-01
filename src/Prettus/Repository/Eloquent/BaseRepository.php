c1d0156f5e4f8ac7747241fc1a41f39438fad37a
Renato Moura <renato@naturalweb.com.br>
2016-09-01 09:20:37 -0300

    applyScope for methods findWhereIn and findWhereNotInt

--- src/Prettus/Repository/Eloquent/BaseRepository.php
+++ src/Prettus/Repository/Eloquent/BaseRepository.php
@@ -415,6 +415,8 @@ abstract class BaseRepository implements RepositoryInterface, RepositoryCriteria
     public function findWhereIn($field, array $values, $columns = ['*'])
     {
         $this->applyCriteria();
+        $this->applyScope();
+
         $model = $this->model->whereIn($field, $values)->get($columns);
         $this->resetModel();
 
@@ -433,6 +435,8 @@ abstract class BaseRepository implements RepositoryInterface, RepositoryCriteria
     public function findWhereNotIn($field, array $values, $columns = ['*'])
     {
         $this->applyCriteria();
+        $this->applyScope();
+        
         $model = $this->model->whereNotIn($field, $values)->get($columns);
         $this->resetModel();
 
