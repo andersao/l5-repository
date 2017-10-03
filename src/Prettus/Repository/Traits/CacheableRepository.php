b82b42eea138d6be96f272a8b542252ee70c5f5f
dmason30 <danmason@tmcapplications.co.uk>
2017-10-03 15:09:29 +0100

    Fixes #446 add reset calls to CacheableRepository

--- src/Prettus/Repository/Traits/CacheableRepository.php
+++ src/Prettus/Repository/Traits/CacheableRepository.php
@@ -208,6 +208,8 @@ trait CacheableRepository
             return parent::all($columns);
         });
 
+        $this->resetModel();
+        $this->resetScope();
         return $value;
     }
 
@@ -232,6 +234,8 @@ trait CacheableRepository
             return parent::paginate($limit, $columns);
         });
 
+        $this->resetModel();
+        $this->resetScope();
         return $value;
     }
 
@@ -255,6 +259,8 @@ trait CacheableRepository
             return parent::find($id, $columns);
         });
 
+        $this->resetModel();
+        $this->resetScope();
         return $value;
     }
 
@@ -279,6 +285,8 @@ trait CacheableRepository
             return parent::findByField($field, $value, $columns);
         });
 
+        $this->resetModel();
+        $this->resetScope();
         return $value;
     }
 
@@ -302,6 +310,8 @@ trait CacheableRepository
             return parent::findWhere($where, $columns);
         });
 
+        $this->resetModel();
+        $this->resetScope();
         return $value;
     }
 
@@ -324,6 +334,8 @@ trait CacheableRepository
             return parent::getByCriteria($criteria);
         });
 
+        $this->resetModel();
+        $this->resetScope();
         return $value;
     }
 }
