3226cc891083b719e05054bcf1a1847f479c2bce
Lucas Moraes (Panik) <lucas.panik@gmail.com>
2017-12-21 15:19:39 -0200

    Refactory Request Criteria
    Modified to fetch only columns that are of search type.

--- src/Prettus/Repository/Criteria/RequestCriteria.php
+++ src/Prettus/Repository/Criteria/RequestCriteria.php
@@ -83,26 +83,28 @@ class RequestCriteria implements CriteriaInterface
                         $relation = implode('.', $explode);
                     }
                     $modelTableName = $query->getModel()->getTable();
-                    if ( $isFirstField || $modelForceAndWhere ) {
-                        if (!is_null($value)) {
-                            if(!is_null($relation)) {
-                                $query->whereHas($relation, function($query) use($field,$condition,$value) {
-                                    $query->where($field,$condition,$value);
-                                });
-                            } else {
-                                $query->where($modelTableName.'.'.$field,$condition,$value);
-                            }
-                            $isFirstField = false;
+                    $findMethod = 'orWhere';
+
+                    if ($isFirstField || $modelForceAndWhere) {
+                        $findMethod = 'where';
+                        $isFirstField = false;
+                    }
+
+                    if ($condition == 'int') {
+                        if (is_numeric($value)) {
+                            $condition = '=';
+                        } else {
+                            continue;
                         }
-                    } else {
-                        if (!is_null($value)) {
-                            if(!is_null($relation)) {
-                                $query->orWhereHas($relation, function($query) use($field,$condition,$value) {
-                                    $query->where($field,$condition,$value);
-                                });
-                            } else {
-                                $query->orWhere($modelTableName.'.'.$field, $condition, $value);
-                            }
+                    }
+
+                    if (!is_null($value)) {
+                        if (!is_null($relation)) {
+                            $query->{$findMethod . 'Has'}($relation, function ($query) use ($field, $condition, $value) {
+                                $query->where($field, $condition, $value);
+                            });
+                        } else {
+                            $query->{$findMethod}($modelTableName.'.'.$field, $condition, $value);
                         }
                     }
                 }
