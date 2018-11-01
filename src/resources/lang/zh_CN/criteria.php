d9bb34535ea47061d7289c6813d3701295ab9633
recca chao <recca.chao@language-center.com.tw>
2018-11-01 10:40:00 +0800

    feat: Add Chinese

---
+++ src/resources/lang/zh_CN/criteria.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'fields_not_accepted' => '栏位 :field 不支援搜寻功能'
+];
---
+++ src/resources/lang/zh_CN/packages.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'prettus_laravel_validation_required' => "需要其它软件包。请安装：'composer require prettus/laravel-validation'",
+    'league_fractal_required'             => "需要其它软件包。请安装：'composer require league/fractal' (0.12.*)"
+];
