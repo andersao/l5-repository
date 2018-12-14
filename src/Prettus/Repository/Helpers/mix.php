e71148bd083acd984a3350fdb58a17fdda8cfd8c
Lloric Mayuga Garcia <lloricode@gmail.com>
2018-12-14 10:58:55 +0800

    add config_path() for lumen

---
+++ src/Prettus/Repository/Helpers/mix.php
@@ -0,0 +1,14 @@
+<?php
+if ( ! function_exists('config_path'))
+{
+    /**
+     * Get the configuration path.
+     *
+     * @param  string $path
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
+    }
+}
