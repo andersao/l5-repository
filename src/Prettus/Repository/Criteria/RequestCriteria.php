684c46aa8c18f2f8ad29c9ee03a65f2af1eba9a6
Stephan de Souza <blad3d@gmail.com>
2019-12-21 19:08:46 -0300

    Add withCount method to Criteria (#561)

--- src/Prettus/Repository/Criteria/RequestCriteria.php
+++ src/Prettus/Repository/Criteria/RequestCriteria.php
@@ -44,6 +44,7 @@ class RequestCriteria implements CriteriaInterface
         $orderBy = $this->request->get(config('repository.criteria.params.orderBy', 'orderBy'), null);
         $sortedBy = $this->request->get(config('repository.criteria.params.sortedBy', 'sortedBy'), 'asc');
         $with = $this->request->get(config('repository.criteria.params.with', 'with'), null);
+        $withCount = $this->request->get(config('repository.criteria.params.withCount', 'withCount'), null);
         $searchJoin = $this->request->get(config('repository.criteria.params.searchJoin', 'searchJoin'), null);
         $sortedBy = !empty($sortedBy) ? $sortedBy : 'asc';
 
@@ -161,6 +162,11 @@ class RequestCriteria implements CriteriaInterface
         if ($with) {
             $with = explode(';', $with);
             $model = $model->with($with);
+        }
+
+        if ($withCount) {
+            $withCount = explode(';', $withCount);
+            $model = $model->withCount($withCount);
         }
 
         return $model;
