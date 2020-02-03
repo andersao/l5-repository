61d38f949d72a073fd56fad9ff760b950bfa2773
ThanhSonITNIC <thanhson.itnic@gmail.com>
2020-02-03 16:29:28 +0700

    add respond

---
+++ src/Prettus/Repository/Traits/Respondable.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Traits;
+
+trait Respondable {
+    /**
+     * Return generic json response with the given data.
+     *
+     * @param $data
+     * @param int $statusCode
+     * @param array $headers
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respond($data, $statusCode = 200, $headers = [])
+    {
+        return response()->json($data, $statusCode, $headers);
+    }
+
+    /**
+     * Respond with success.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondSuccess()
+    {
+        return $this->respond(null);
+    }
+
+    /**
+     * Respond with created.
+     *
+     * @param $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondCreated($data)
+    {
+        return $this->respond($data, 201);
+    }
+
+    /**
+     * Respond with no content.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondNoContent()
+    {
+        return $this->respond(null, 204);
+    }
+
+    /**
+     * Respond with error.
+     *
+     * @param $message
+     * @param $statusCode
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondError($message, $statusCode)
+    {
+        return $this->respond([
+            'errors' => [
+                'message' => $message,
+                'status_code' => $statusCode
+            ]
+        ], $statusCode);
+    }
+
+    /**
+     * Respond with unauthorized.
+     *
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondUnauthorized($message = 'Unauthorized')
+    {
+        return $this->respondError($message, 401);
+    }
+
+    /**
+     * Respond with forbidden.
+     *
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondForbidden($message = 'Forbidden')
+    {
+        return $this->respondError($message, 403);
+    }
+
+    /**
+     * Respond with not found.
+     *
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondNotFound($message = 'Not Found')
+    {
+        return $this->respondError($message, 404);
+    }
+
+    /**
+     * Respond with internal error.
+     *
+     * @param string $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondInternalError($message = 'Internal Error')
+    {
+        return $this->respondError($message, 500);
+    }
+
+}
